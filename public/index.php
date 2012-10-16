196248fc3bc26ec850c93c59262f841d7b3d4068
Kathryn Reeve <k.reeve@ctidigital.com>
2012-10-16 02:01:41 +0100

    removed init_autoloader and fixed requirement as per comment

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 chdir(dirname(__DIR__));
-include 'init_autoloader.php';
+require 'vendor/autoload.php';
 
 $app = \ZeffMu\App::init();
 $app
@@ -10,10 +10,10 @@ $app
     ->route('/hello', function() {
         return 'Hi!';
     })
-    ->route('/hello/:name', function($params) use ($app) {
+    ->route('/hello/:name', function($params) {
         return 'Hello ' . $params['name'];
     })
-    ->route('/hello/:name/:surname', function($params) use ($app) {
+    ->route('/hello/:name/:surname', function($params) {
         return 'Hello, Mr. ' . $params['surname'] . ', or shall I call you ' . $params['name'] . '?';
     })
 ->run();
