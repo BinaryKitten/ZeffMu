b270c6afda161572792c81c8ecfea667fe0b736b
Marco Pivetta <ocramius@gmail.com>
2013-01-13 03:24:03 +0100

    Fixing broken test caused by annoying SendResponseListener

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,14 +1,17 @@
 <?php
 
 if (is_readable(__DIR__ . '/../vendor/autoload.php')) {
-    require_once __DIR__ . '/../vendor/autoload.php';
+    $loader = require __DIR__ . '/../vendor/autoload.php';
 } else {
     if (!is_readable(__DIR__ . '/../../../autoload.php')) {
         throw new RuntimeException('Please run composer installation!');
     }
 
-    require_once __DIR__ . '/../../../autoload.php';
+    $loader = require __DIR__ . '/../../../autoload.php';
 }
+
+$loader->add('ZeffMuTest\\', __DIR__);
+$loader->add('ZeffMuTestAsset\\', __DIR__);
 
 if (is_readable(__DIR__ . DIRECTORY_SEPARATOR . 'TestConfiguration.php')) {
     $config =  __DIR__ . DIRECTORY_SEPARATOR . 'TestConfiguration.php';
