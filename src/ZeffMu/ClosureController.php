369eb0510f72e1c076217a3f16610c64a351c917
Kathryn Reeve <Kathryn@BinaryKitten.com>
2013-01-17 20:32:11 +0000

    update to ClosureController to process content

--- src/ZeffMu/ClosureController.php
+++ src/ZeffMu/ClosureController.php
@@ -1,15 +1,33 @@
 <?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://github.com/BinaryKitten/ZeffMu>.
+ */
 
 namespace ZeffMu;
 
 use Zend\Mvc\Controller\AbstractController;
 use Zend\Mvc\MvcEvent;
+use Zend\View\Model\ViewModel;
 use Closure;
 
 /**
  * Description of ClosureController
  *
- * @author Kat
+ * @author Kathryn Reeve <Kathryn@BinaryKitten.com>
  */
 class ClosureController extends AbstractController
 {
@@ -19,11 +37,20 @@ class ClosureController extends AbstractController
      */
     protected $closure = null;
 
+    /**
+     * Constructor
+     * @param Closure $closure
+     */
     public function __construct(Closure $closure)
     {
         $this->closure = $closure;
     }
 
+    /**
+     * 
+     * @param \Zend\Mvc\MvcEvent $e
+     * @return misc
+     */
     public function onDispatch(MvcEvent $e)
     {
         $routeMatch     = $e->getRouteMatch();
@@ -35,6 +62,13 @@ class ClosureController extends AbstractController
         if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
             $closure->bindTo($this);
         }
-        return $closure($routeMatch->getParams(), $request, $response);
+        $result         = $closure(
+            $routeMatch->getParams(),
+            $request,
+            $response
+        );
+
+        $e->setResult($result);
+        return $result;
     }
 }
